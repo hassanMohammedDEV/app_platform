15f61bbbb0fa8dffa6f9c5c0a034a0acdea595b2
hassanWork <h714241926@gmail.com>
2026-05-06 14:23:37 +0300

    change validations classes.

--- packages/state/lib/src/form/validator_type.dart
+++ packages/state/lib/src/form/validator_type.dart
@@ -15,3 +15,9 @@ String? Function(ValidationContext<K> context);
 
 typedef AsyncValidator<K extends Enum> =
 Future<String?> Function(ValidationContext<K> context);
+
+typedef FieldValidator<TValue> =
+String? Function(TValue value);
+
+typedef FormValidator<TData> =
+String? Function(TData data);
